bc4b7fae971257280449cc942a4b9284a72f3a04
Blue <82478297+Yagoi23@users.noreply.github.com>
2021-05-29 11:01:59 +1200

    enemys jumping towards

--- BestGroupGameJam2021/Enemy/Enemy.gd
+++ BestGroupGameJam2021/Enemy/Enemy.gd
@@ -2,13 +2,15 @@ extends KinematicBody
 
 var speed = 100
 var space_state
-var target
+onready var player = get_parent().get_parent().get_node("Player")
 var health = 3
 var can_hit
+var airtime = 0
 
 onready var blood = preload("res://ParticleEffects/SnailBlood.tscn")
 
 func _ready():
+	print(player)
 	space_state = get_world().direct_space_state
 
 func hit_enemy():
@@ -27,27 +29,24 @@ func hit_player():
 		get_tree().change_scene("res://Ui/DeathScreen/DeathScreen.tscn")
 
 func _process(delta):
-	if target:
-		var result = space_state.intersect_ray(global_transform.origin, target.global_transform.origin)
-		if result.collider.is_in_group("Player"):
-			look_at(target.global_transform.origin, Vector3.UP)
-			var direction = (target.transform.origin - transform.origin).normalized()
-			direction.y += 10
-			move_and_slide(direction * speed * delta, Vector3.UP)
-
-func _on_Area_body_entered(body):
-	if body.is_in_group("Player"):
-		target = body
-		print(body.name + " entered")
-
-func _on_Area_body_exited(body):
-	if body.is_in_group("Player"):
-		target = null
-		print(body.name + " exited")
+	if not player:
+		return
+	if player.transform.origin.distance_to(transform.origin) <= 2:
+		return
+	var move = Vector3(0,0,0)
+	look_at(player.global_transform.origin, Vector3.UP)
+	rotation_degrees.x = 0
+	var direction = (player.transform.origin - transform.origin).normalized()
+	if is_on_floor():
+		airtime = 15
+	else:
+		airtime -= 0.75
+	direction.y += airtime
+	move_and_slide(direction * delta * speed, Vector3.UP)
 
 func _on_HitZone_body_entered(body):
 	if body.is_in_group("Player") and $HitTimer.is_stopped():
-		$HitTimer.start(1)
+		$HitTimer.start(2)
 		can_hit = true
 
 func _on_HitTimer_timeout():
