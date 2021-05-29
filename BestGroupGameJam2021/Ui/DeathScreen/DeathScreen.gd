6c155463c910a7cf472c8e44ffaf3b41566707be
Blue <82478297+Yagoi23@users.noreply.github.com>
2021-05-29 13:52:47 +1200

    high score added

--- BestGroupGameJam2021/Ui/DeathScreen/DeathScreen.gd
+++ BestGroupGameJam2021/Ui/DeathScreen/DeathScreen.gd
@@ -1,27 +1,15 @@
 extends Control
 
+var current = 0
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	get_tree().set_auto_accept_quit(false)
-	#new_hight_score(1,"kiwi2") 
-
+	$YouDied/Add_score/Currnet.text = "Current:" + str(current)
+	show_high_score() 
 
 func _on_Exit_pressed():
 	get_tree().change_scene("res://Ui/TitleScreen/Title.tscn")
-#	pass # Replace with function body.
-
 
 func _on_NewGame_pressed():
-#	PlayerInfo.health = 100 / PlayerInfo.difficulty
-#	PlayerInfo.charges = 100 / PlayerInfo.difficulty
-#	PlayerInfo.charges_clip = 100 / PlayerInfo.difficulty
-#	PlayerInfo.power = 100 / PlayerInfo.difficulty
 	PlayerInfo.reset_level()
 	PlayerInfo.weapon = false
 	PlayerInfo.health = 100
@@ -29,14 +17,13 @@ func _on_NewGame_pressed():
 	PlayerInfo.charges_clip = 100
 	PlayerInfo.power = 100
 	get_tree().change_scene("res://Levels/Level1.tscn")
-#	print("yes")
-#	pass # Replace with function body.
 
-func new_hight_score(number,name):
+func add_hight_score(number,name):
+	$YouDied/Add_score/Add_score_button.disabled = true
+	$YouDied/Add_score/Add_score_button.visible = false
+	$YouDied/Add_score/TextEdit.visible = false
+	$YouDied/Add_score/Name.visible = false
 	var save = load("user://save.res")
-	if not save:
-		save = Resource.new()
-		save.set_script(load("res://Ui/HighscoreWindow/High_score_save.gd"))
 	var added = false
 	for p in range(len(save.places)):
 		if save.places[p].score <= number:
@@ -46,8 +33,22 @@ func new_hight_score(number,name):
 	if not added:
 		save.places.append({"name":name,"score":number})
 	print(ResourceSaver.save("user://save.res",save))
+	show_high_score()
+
+func show_high_score():
+	var save = load("user://save.res")
+	if not save:
+		save = Resource.new()
+		save.set_script(load("res://Ui/HighscoreWindow/High_score_save.gd"))
+		print(ResourceSaver.save("user://save.res",save))
+	
+	for l in $HighScores/Box.get_children():
+		l.queue_free()
+	
 	for p in save.places:
 		var lab = Label.new()
 		lab.text = p.name + ":" + str(p.score)
 		$HighScores/Box.add_child(lab)
-	#$HighScores/Currnet.text = "Current:" + str(number)
+
+func _on_Add_score_button_down():
+	add_hight_score(current,$YouDied/Add_score/TextEdit.text)
