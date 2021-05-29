d1e5564364c6067619c4653e37b2d1a3bf326d9f
Blue <82478297+Yagoi23@users.noreply.github.com>
2021-05-29 16:45:43 +1200

    Fix to player

--- BestGroupGameJam2021/Player/FlashLight.gd
+++ BestGroupGameJam2021/Player/FlashLight.gd
@@ -11,19 +11,6 @@ func _ready():
 	visible = false
 	pass # Replace with function body.
 
-func _input(event):
-	if not PlayerInfo.power <= 0:
-		if Input.is_action_just_pressed("flashlight") and visible == true:
-			visible = false
-#			print("flashlight off")
-		elif Input.is_action_just_pressed("flashlight") and visible == false:
-			visible = true
-	elif PlayerInfo.power <= 0:
-		visible = false
-#			print("flashlight on")
-	
-
-
 func _on_PowerDrain_timeout():
 	if visible == true:
 		PlayerInfo.change_power(-1)
