da4e2e7fcd4db1bfaca72699dcc955c9d3055aaf
Blue <82478297+Yagoi23@users.noreply.github.com>
2021-05-30 11:36:51 +1200

    fixed sound play when no charge

--- BestGroupGameJam2021/Player/Player.gd
+++ BestGroupGameJam2021/Player/Player.gd
@@ -65,18 +65,18 @@ func _physics_process(delta):
 
 
 	if Input.is_action_pressed("primary_fire") and PlayerInfo.score >= 20100:
-		$LazerSound.play()
 		if PlayerInfo.charge >= 1:
 			PlayerInfo.change_charge(-1)
+			$LazerSound.play()
 			var lazer = load("res://Player/LaserMesh.tscn").instance()
 			lazer.move = -($Head/Camera.global_transform.origin-$Head/Camera/Spatial.global_transform.origin).normalized()
 			lazer.look_at(lazer.move,Vector3.UP)
 			lazer.translation = global_transform.origin + Vector3(0,0.5,0)
 			get_parent().get_node("Lazers").add_child(lazer)
-	elif Input.is_action_just_pressed("primary_fire"):
-		$LazerSound.play()
+	elif Input.is_action_just_pressed("primary_fire"):		
 		if PlayerInfo.charge >= 10:
 			PlayerInfo.change_charge(-10)
+			$LazerSound.play()
 			var lazer = load("res://Player/LaserMesh.tscn").instance()
 			lazer.move = -($Head/Camera.global_transform.origin-$Head/Camera/Spatial.global_transform.origin).normalized()
 			lazer.look_at(lazer.move,Vector3.UP)
