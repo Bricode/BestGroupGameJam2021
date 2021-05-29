4bcad161903e7e19b8ce07a4539ea198af1b1a72
Blue <82478297+Yagoi23@users.noreply.github.com>
2021-05-30 10:16:48 +1200

    grub fix and carmar shake

--- BestGroupGameJam2021/GameModels/Items/HuhuGrub.gd
+++ BestGroupGameJam2021/GameModels/Items/HuhuGrub.gd
@@ -1,22 +1,6 @@
-extends RigidBody
-
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+extends KinematicBody
 
 func _on_Area_body_shape_entered(body_id, body, body_shape, area_shape):
-	print(body)
 	if body.is_in_group("Player"):
 		if PlayerInfo.charge <= 99:
 			PlayerInfo.change_charge(50)
@@ -25,13 +9,10 @@ func _on_Area_body_shape_entered(body_id, body, body_shape, area_shape):
 			
 func newlocation():
 	hide()
-	var newzpos = rand_range(-1.5,1.5)
-	var newxpos = rand_range(-1.5,1.5)
-	translation = Vector3(newxpos,0.5,newzpos)
 	$Timer.start()
 	
-
-
-
 func _on_Timer_timeout():
 	show()
+
+func _physics_process(delta):
+	move_and_collide(Vector3(0,-5,0)* delta)
--- BestGroupGameJam2021/Player/Player.gd
+++ BestGroupGameJam2021/Player/Player.gd
@@ -14,7 +14,13 @@ onready var camera = $Head/Camera
 var velocity = Vector3()
 var camera_x_rotation = 0
 
+var trauma = 0.0
+var decay = 0.8
+var trauma_power = 2
+var max_offset = Vector2(0.75,0.75)
+
 func _ready():
+	randomize()
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _input(event):
@@ -85,4 +91,17 @@ func _physics_process(delta):
 	if PlayerInfo.konami_code:
 		PlayerInfo.score += 10
 		PlayerInfo.charge = 100
+	if trauma:
+		trauma = max(trauma - decay * delta, 0)
+		shake()
 
+func hit():
+	trauma = 1
+	PlayerInfo.change_health(-10)
+	if PlayerInfo.health <= 0:
+		get_tree().change_scene("res://Ui/DeathScreen/DeathScreen.tscn")
+
+func shake():
+	var amount = pow(trauma, trauma_power)
+	$Head/Camera.h_offset = max_offset.x * amount * rand_range(-1, 1)
+	$Head/Camera.v_offset = max_offset.y * amount * rand_range(-1, 1)
