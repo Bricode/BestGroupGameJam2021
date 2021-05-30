0ad2a3584e232db1342f7bb75298f27f1b8b03f2
Yagoi <55006658+bluewolf44@users.noreply.github.com>
2021-05-30 15:24:13 +1200

    last push

--- BestGroupGameJam2021/Ui/HighscoreWindow/High_score_save.gd
+++ BestGroupGameJam2021/Ui/HighscoreWindow/High_score_save.gd
@@ -2,5 +2,10 @@ extends Resource
 class_name Save
 
 export var places = [
-	{"name":"kiwi","score":100},
+	{"name":"James","score":22860}, 
+	{"name":"Blake","score":12000}, 
+	{"name":"Daniel","score":11420}, 
+	{"name":"Tiaan","score":8000},
+	{"name":"Chase","score":4200},
+	{"name":"Isaac","score":1},
 ]
